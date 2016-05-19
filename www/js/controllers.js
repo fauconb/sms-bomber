e8db47f61453f0f1d18cf00d697adc3228bd90d6
faucon_b <faucon_b>
2016-05-19 15:08:19 +0200

    Push Css, Loader, .Apk

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('SpamCtrl',function($scope,$cordovaSms, $timeout, $ionicLoading){
+.controller('SpamCtrl',function($scope,$cordovaSms, $timeout, $ionicLoading, $ionicPopup){
 
   document.addEventListener("deviceready", function() {
     $ionicLoading.show({
@@ -9,29 +9,30 @@ angular.module('starter.controllers', [])
 
     $scope.contacts = [];
     navigator.contacts.find(
-        [navigator.contacts.fieldType.displayName],
-        gotContacts,
-        errorHandler);
+      [navigator.contacts.fieldType.displayName],
+      gotContacts,
+      errorHandler);
 
-        function errorHandler(e) {
-            alert("errorHandler: "+e);
-        }
+    function errorHandler(e) {
+      alert("errorHandler: "+e);
+    }
 
-        function gotContacts(c) {
-            var contacts_filter = [];
-            for (var i = 0, len = c.length; i < len; i++) {
-              if (c[i].phoneNumbers && c[i].displayName) {
-                contacts_filter.push({ name: c[i].displayName, number: c[i].phoneNumbers[0].value });
-              }
-            }
-            $scope.contacts = contacts_filter;
-            $ionicLoading.hide();
+    function gotContacts(c) {
+      var contacts_filter = [];
+      for (var i = 0, len = c.length; i < len; i++) {
+        if (c[i].phoneNumbers && c[i].displayName) {
+          contacts_filter.push({ name: c[i].displayName, number: c[i].phoneNumbers[0].value });
         }
+      }
+      $scope.contacts = contacts_filter;
+      $ionicLoading.hide();
+    }
 
     $scope.sms_options={};
     $scope.sms_sent = 0;
     $scope.sms_error = 0;
     $scope.is_sending_sms = false;
+    $scope.sms_options.number = '';
 
     $scope.bombSms = function () {
       if ($scope.is_sending_sms) {
@@ -40,7 +41,7 @@ angular.module('starter.controllers', [])
       $scope.is_sending_sms = true;
 
       for (var i = 0; i < $scope.sms_options.times; ++i) {
-        $scope.sendSms($scope.sms_options.delay, i, $scope.sms_options.times, $scope.sms_options.number);
+        $scope.sendSms($scope.sms_options.delay * 1000, i, $scope.sms_options.times, $scope.sms_options.number);
       }
     };
 
@@ -48,22 +49,33 @@ angular.module('starter.controllers', [])
       $scope.sms_options.number = $scope.sms_options.contact;
     };
 
-  $scope.sendSms = function (delay, i, times, number) {
-    $timeout(function() {
-      $cordovaSms
-      .send(number, $scope.sms_options.message)
+    $scope.sendSms = function (delay, i, times, number) {
+
+      $timeout(function() {
+        $cordovaSms
+        .send(number, $scope.sms_options.message)
         .then(function() {
-            $scope.sms_sent++;
+          $scope.sms_sent++;
         }, function(error) {
-            $scope.sms_error++;
-      });
-      if (i == times - 1) {
-        alert(times + ' Messages sent to ' + number);
+          $scope.sms_error++;
+        });
+        if ($scope.is_sending_sms === true) {
+          $ionicLoading.show({
+            template: '<img class="loadingSms" src="img/icon.png" alt="img" /><p>Sending Sms ...</p> '
+          });
+        }
+        if (i == times - 1) {
+        // alert(times + ' Messages sent to ' + number);
+        $ionicLoading.hide();
+        $ionicPopup.alert({
+         title: 'Success - '+$scope.sms_options.number,
+         template: '<p>' + $scope.sms_sent + ' sent. </p><p>'+ $scope.sms_error + ' not sent.</p>'
+       });
         $scope.sms_sent = 0;
         $scope.sms_error = 0;
         $scope.is_sending_sms = false;
       }
     }, delay * i);
-  };
-});
+    };
+  });
 });
